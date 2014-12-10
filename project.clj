ad310f43b2c13ef7dc701c25c8c60d5b739c6413
Murphy McMahon <pandeiro@gmail.com>
2014-12-10 15:49:49 -0200

    update clojure dep

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2138"]])
