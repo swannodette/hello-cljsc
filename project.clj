efe910f59e2ccf06163e93b9be783c98e71cc2af
Murphy McMahon <pandeiro@gmail.com>
2014-12-10 15:50:05 -0200

    update clojurescript dep

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2138"]])
+                 [org.clojure/clojurescript "0.0-2411"]])
